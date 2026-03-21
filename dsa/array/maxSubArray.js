d6192114b7ee38dbb3013b1057db98e462551b84
Vamshi Krishna Kodem <139354055+vamshiKodem@users.noreply.github.com>
2026-03-21 18:03:53 +0100

    Create maxSubArray.js

---
+++ dsa/array/maxSubArray.js
@@ -0,0 +1,16 @@
+const nums = [1,2,3,4];
+
+const maxSubArray = (nums) => {
+  let sum = nums[0];
+  let currentSum = nums[0];
+  for(let i = 1; i < nums.length; i++){
+    currentSum = Math.max(currentSum, currentSum + nums[i]);
+    sum = Math.max(currentSum, sum);
+  }
+  return sum;
+}
+
+console.log(maxSubArray(nums)); // 10
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // 6 ([4,-1,2,1])
+console.log(maxSubArray([1,2,3,4]));               // 10 ([1,2,3,4])
+console.log(maxSubArray([-1,-2,-3]));              // -1 ([-1])
